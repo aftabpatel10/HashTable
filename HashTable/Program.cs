a4dbea20afbf1b10ae4591991c1f284730cce6e0
Patel <aftabkpatel10@gmail.com>
2022-12-09 09:20:20 +0530

    Removing Word From Paragraph

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static HashTable.MyMapNode1;
 
 namespace HashTable
 {
@@ -44,6 +45,29 @@ namespace HashTable
                         "deliberately into paranoid avoidable " +
                         "situations";
                     CountNumbOfOccurence(paragraph1);
+                    break;
+                case 3:                     //UC3 (Counting frequency of occurrence of words in a paragraph and removing 1 word and showing difference.
+                    MyMapNode<string, int> hashTabe = new MyMapNode<string, int>(6);
+                    string paragraph2 = "Paranoids are not paranoid " +
+                        "because they are paranoid but " +
+                        "because they keep putting themselves " +
+                        "deliberately into paranoid avoidable " +
+                        "situations";
+                    string[] words = paragraph2.Split(' ');
+                    foreach (string word in words)
+                    {
+                        if (hashTabe.Exists(word.ToLower()))
+                            hashTabe.Add(word.ToLower(), hashTabe.Get(word.ToLower()) + 1);
+                        else
+                            hashTabe.Add(word.ToLower(), 1); //to,1 
+                    }
+                    Console.WriteLine("All words counted .");
+                    hashTabe.Display();
+                    string s = "avoidable";
+                    hashTabe.Remove(s);
+                    Console.WriteLine();
+                    Console.WriteLine($"A word \"{s}\" has been removed ");
+                    hashTabe.Display();
                     break;
                 default:
                     Console.WriteLine("Invalid option selected ! Please try again .");
